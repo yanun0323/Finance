1e92374d78c31c633402c7159e1a2bed001d8826
yanun0323 <a1256212562@gmail.com>
2022-06-15 23:42:50 +0800

    Initial Commit

---
+++ Finance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finance
+//
+//  Created by Yanun on 2022/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finance/FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceApp.swift
+//  Finance
+//
+//  Created by Yanun on 2022/6/15.
+//
+
+import SwiftUI
+
+@main
+struct FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
